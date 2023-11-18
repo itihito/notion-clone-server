b76414620f12a45d8c5ae231848845657aa8be50
itihito <itihito>
2023-11-19 08:51:55 +0900

    ミドルウェアのファイル名修正

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = 5000;
 const origin = IS_PRODUCTION ? process.env.ORIGIN : "http://127.0.0.1:5173";
 const cors = require("cors");
-const logOriginMiddleware = require("./src/v1/middlewares/middleware.js");
+const logOriginMiddleware = require("./src/v1/middlewares/logRequestMiddleware.js");
 
 // ミドルウェアを設定
 app.use(logOriginMiddleware);
